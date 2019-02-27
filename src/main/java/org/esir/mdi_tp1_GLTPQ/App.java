9f8efc86326f82087281b1918d432f05153b9e3d
Gatien <gatien.gaumerais@gmail.com>
2019-02-27 11:47:34 +0100

    [CHANGE] <.gitignore + implemented calc choice> <Added .gitignore and an initial implementation of the calculator (initial project, .gitignore and initial implementation)> This commit does the following: * Add files to ignore to .gitignore * Implemented calculator choice in App.java

--- src/main/java/org/esir/mdi_tp1_GLTPQ/App.java
+++ src/main/java/org/esir/mdi_tp1_GLTPQ/App.java
@@ -1,5 +1,7 @@
 package org.esir.mdi_tp1_GLTPQ;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String calc = "";
+    	System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Entrez le type de calculatrice désiré :");
+        System.out.println("1 : Normale");
+        System.out.println("2 : Scientifique");
+        int type = sc.nextInt();
+        
+        if(type == 1) {
+        	calc = "normale";
+        }
+        else if(type == 2) {
+        	calc = "scientifique";
+        }
+        
+        sc.close();
+        System.out.println("Type choisi : "+calc);
     }
 }

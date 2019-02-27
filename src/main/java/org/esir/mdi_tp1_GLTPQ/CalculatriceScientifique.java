ea397bbafd2b368df3bd1c3bafbe0042d1bc5582
Gatien <gatien.gaumerais@gmail.com>
2019-02-27 15:06:33 +0100

    Added puissance and showLastResult in CalculatriceScientifique

--- src/main/java/org/esir/mdi_tp1_GLTPQ/CalculatriceScientifique.java
+++ src/main/java/org/esir/mdi_tp1_GLTPQ/CalculatriceScientifique.java
@@ -2,4 +2,18 @@ package org.esir.mdi_tp1_GLTPQ;
 
 public class CalculatriceScientifique {
 
+	double m_last_result;
+	
+	public double puissance(double nombre, double puissance) {
+		
+		m_last_result = Math.pow(nombre, puissance);
+		
+		return m_last_result;
+		
+	}
+	
+	public double getLastResult() {
+		return m_last_result;
+	}
+	
 }

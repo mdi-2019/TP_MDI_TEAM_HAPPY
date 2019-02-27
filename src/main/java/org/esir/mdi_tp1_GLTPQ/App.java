9a1c3582240a55af79e1315284178f51ba74afb4
Gatien <gatien.gaumerais@gmail.com>
2019-02-27 11:06:54 +0100

    [CHANGE] <Initial commit> <This commit inits the git (nothing, init)> This commit does the following: Initialize the git

---
+++ src/main/java/org/esir/mdi_tp1_GLTPQ/App.java
@@ -0,0 +1,13 @@
+package org.esir.mdi_tp1_GLTPQ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
